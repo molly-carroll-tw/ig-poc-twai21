00079cabee21e7a96ec3f7755feaa8f6df3db36b
Molly Carroll <molly.carroll@thoughtworks.com>
2024-05-06 10:24:06 -0600

    edit some comments for sync

--- ingest_to_library.py
+++ ingest_to_library.py
@@ -9,14 +9,14 @@ api_key = os.environ["AI21_API_KEY"]
 url = "https://api.ai21.com/studio/v1/library/files"
 headers = {"Authorization": f"Bearer {api_key}"}
 
-files = {"file": open("data/Profile2.pdf", "rb"), "file2": open("data/Profile1.pdf", "rb")}
+files = {"file": open("data/Transcript_Mock_Interview.docx", "rb")}
 # data = {
 #     "path": "lib_data/interview_data",
 # }
-
+    
 requests.post(url=url, headers=headers, files=files)
 
-# result: can POST multiple files to the library
+# result: can POST multiple files to the library, but only 1 at a time! (unless I did open() syntax wrong)
 
 # QUESTIONS:
 # Are the documents transformed (index calculation, embedding of text) at all when they're opened or used in the request? making REST call via Python SDK
